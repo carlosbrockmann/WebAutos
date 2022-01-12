08c8dbf9159fff820bc19b51d878408a405c28d7
carlosbrockmann <cjbrockmann@topmail-files.de>
2022-01-12 17:04:10 +0100

    1 file modified, 1 added

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './sites/home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'autos',
+    loadChildren: () =>
+      import('./sites/autos/autos.module').then((m) => m.AutosModule),
+  },
+  { path: '', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
 })
-export class RoutingModule { }
+export class RoutingModule {}
