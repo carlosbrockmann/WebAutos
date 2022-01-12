1705ba7d69ad346cb9acd73514361af45ca55dda
carlosbrockmann <cjbrockmann@topmail-files.de>
2022-01-12 14:12:39 +0100

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    NgxNavbarModule,
+  ],
+  declarations: [AppComponent, HelloComponent, MenuComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
