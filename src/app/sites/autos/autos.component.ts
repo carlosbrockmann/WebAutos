7788b646768aacffdd5cfb6931b66dec6cfab0d1
carlosbrockmann <cjbrockmann@topmail-files.de>
2022-01-12 21:39:55 +0100

    2 files modified

--- src/app/sites/autos/autos.component.ts
+++ src/app/sites/autos/autos.component.ts
@@ -6,11 +6,28 @@ import { GlobalConstants, CARS } from '../../global-constants';
   templateUrl: './autos.component.html',
   styleUrls: ['./autos.component.css'],
 })
+
+static automarken: string[];
+
 export class AutosComponent implements OnInit {
   autoListe: CARS[];
+  
 
   constructor() {
     this.autoListe = GlobalConstants.CarList;
+
+    const marken = []; 
+
+    if (this.autoListe) {
+      
+      // this.autoListe.forEach(function (obj, i) {
+      //   if (obj.Marke && marken.indexOf(obj.Marke) < 0)
+      //   marken.push(obj.Marke);
+      // });
+      //   this.autoListe.forEach(function (v) {
+      //     if (!this.automarken.icludes(v.Marke)) this.automarken.push(v.Marke);
+      //   });
+    }
   }
 
   ngOnInit() {}
